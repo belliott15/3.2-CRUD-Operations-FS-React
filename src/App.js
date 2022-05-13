9ddaddeeb00546e8145d37e30582373ce9cb1c3f
Beau Elliott <belliott1516@gmail.com>
2022-05-12 17:53:06 -0700

    created skeleton for App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+import AuthPage from './AuthPage';
+import BookList from './BookList';
+import CreateBook from './CreateBook';
+import UpdateBook from './UpdateBook';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/books'>Home</Link>
+            </li>
+            <li>
+              <Link to='/add'>Add Book</Link>
+            </li>
+            <li>
+              <Link to='/update'>Update Book</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path='/'>
+            <AuthPage />
+          </Route>
+          <Route path='/books'>
+            <BookList />
+          </Route>
+          <Route path='/add'>
+            <CreateBook />
+          </Route>
+          <Route path='/update'>
+            <UpdateBook />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
